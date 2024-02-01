4bbb110d6353869aa97ec372ff67adfc4efff210
Tedros Alem <tedsaasfaha@gmail.com>
2024-02-01 21:50:35 +0300

    implementing and testing dynamic templates with dummy data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        "author": "John Doe",
+        "title": "Blog Post 1",
+        "content": "This is the content of blog post 1",
+        "date_posted": "May 24, 1991",
+    },
+    {
+        "author": "Jane Smith",
+        "title": "blog Post 2",
+        "content": "This is the content of blog post 2.",
+        "date_posted": "January 10, 2024",
+    },
+]
+
+
 def home(request):
-    return render(request, "blog/home.html")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", {"title": "About"})
