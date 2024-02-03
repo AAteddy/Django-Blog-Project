ecaae0c0007fd614eac819613d5631690de63080
Tedros Alem <tedsaasfaha@gmail.com>
2024-02-04 01:47:42 +0300

    error solved by installing crispy_bootstrap4 third party app

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "blog.apps.BlogConfig",
     "users.apps.UsersConfig",
     "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -126,4 +127,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
